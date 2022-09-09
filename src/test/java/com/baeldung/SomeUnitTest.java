8f5e57e5773f6e0e9d36b82fd01ff103a8172b42
valzkat1 <valzkat12@gmail.com>
2022-09-09 04:49:58 -0500

    Update SomeUnitTest.java

--- src/test/java/com/baeldung/SomeUnitTest.java
+++ src/test/java/com/baeldung/SomeUnitTest.java
@@ -3,9 +3,9 @@ package com.baeldung;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
-import  hudson.plugins.checkstyle;
 
-public class SomeUnitTest implements SimpleBuildStep {
+
+public class SomeUnitTest{
     @Test
     public void init() {
         assertEquals(1, 1);
