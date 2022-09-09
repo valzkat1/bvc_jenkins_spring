735a92efa478f418aa920d096b80918b864e453d
valzkat1 <valzkat12@gmail.com>
2022-09-09 04:37:00 -0500

    Update SomeUnitTest.java

--- src/test/java/com/baeldung/SomeUnitTest.java
+++ src/test/java/com/baeldung/SomeUnitTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import  hudson.plugins.checkstyle;
 
-public class SomeUnitTest extends CheckStylePublisher{
+public class SomeUnitTest implements SimpleBuildStep {
     @Test
     public void init() {
         assertEquals(1, 1);
